7bff8e04b771bebd04176b7c4dc3557c5af5a118
Zayyan Faizal <zayyan@usedispatch.xyz>
2024-10-23 23:19:21 -0500

    remove old components and app

--- ui/components-1/link.tsx
+++
@@ -1,8 +0,0 @@
-import Link1, { LinkProps } from "next/link"
-import { Link as Link2, LinkProps as ReactLinkProps, To } from "react-router-dom"
-
-export default function Link(props: ReactLinkProps & LinkProps) {
-  const isArweave = process.env.NEXT_PUBLIC_DEPLOY_TARGET === "arweave"
-  if (isArweave) props.to = props.href as To
-  return isArweave ? <Link2 {...props} /> : <Link1 {...props} />
-}
